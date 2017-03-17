2de575526234965c55cec24817202dcd2ba41f27
Aditya Jain <adityajain.aj10@gmail.com>
2017-03-17 12:23:14 +0530

    added config details
    removed username and password

---
+++ sep/src/_mail__/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// contains PHPMailer - SMTP configuration
+// #defines
+
+define('SMTP_USERNAME', '');
+define('SMTP_PASSWORD', '');
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_FROMNAME', 'IITI LPS Notifications');
+
+?>
