20dd9987943ed0f82a202065d4118e2e91f6a913
Aditya Jain <adityajain.aj10@gmail.com>
2017-04-01 22:49:58 +0530

    gui bug fixed

--- sep/src/public/view_app_apr.php
+++ sep/src/public/view_app_apr.php
@@ -170,54 +170,13 @@
                         
                     </div>
                     <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-6 -->
-                <!--div class="col-lg-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                        <!-- add this attribute to button to open in new tab target="_blank" -->
-                            <!--a class="btn btn-default btn-md btn-block"  href="../reject_app/<?php echo $rec['application_id'] ?>"><b>Reject</b></a>
-                        </div>
-                       
-                </div-->
-                <!-- /.col-lg-6 -->
+            </div>
             </div>
             <!-- /.row -->
                 </div>
                 </div>
 
-            <!--
-            <table>
-                <tr>
-                    <th> Field </th>
-                    <th> Data </th>
-                </tr>
-                <tr>
-                    <td> <b> Application </b> </td>
-                    <td>  </td>
-                </tr>
-            </table>
-            <!--
-            <b> Applicati
-        -->
-
     
-
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="../../vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- DataTables JavaScript -->
-    <script src="../../vendor/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="../../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
-    <script src="../../vendor/datatables-responsive/dataTables.responsive.js"></script>
 
     <!-- Custom Theme JavaScript -->
     <script src="../../dist/js/sb-admin-2.js"></script>
