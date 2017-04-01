c861e6d1af2e441406fb7c23a8e37267b1998e80
Aditya Jain <adityajain.aj10@gmail.com>
2017-04-01 12:58:35 +0530

    minor gui fixes

--- sep/src/public/view_app_apr.php
+++ sep/src/public/view_app_apr.php
@@ -12,22 +12,22 @@
     <title>IITI LPS</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/sep-2/src/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="/sep-2/src/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="../../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- DataTables CSS -->
-    <link href="/sep-2/src/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="../../vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
 
     <!-- DataTables Responsive CSS -->
-    <link href="/sep-2/src/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+    <link href="../../vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/sep-2/src/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../../dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="/sep-2/src/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -43,7 +43,7 @@
     <div id="wrapper">
 
         <!-- Navigation and Side bars-->
-        <?php include 'bars.php' ?>
+        <?php include 'bars_internal.php' ?>
         <div id="page-wrapper">
             <!-- add content here -->
             <div class="row">
@@ -121,7 +121,7 @@
                                     </tr>
                                     <tr class="odd gradeX">
                                     <td> <b> Approving Authority </b> </td>
-                                    <td> <?php echo $rec['recommending_auth'] ?> </td>
+                                    <td> <?php echo $rec['approving_auth'] ?> </td>
                                     </tr>
                                     <tr class="odd gradeX">
                                     <td> <b> Status </b> </td>
@@ -139,7 +139,6 @@
                                     <td> <b> Approving Authority Remark </b> </td>
                                     <td> <?php echo $rec['approver_comments'] ?> </td>
                                     </tr>
-
                                 </tbody>
                             </table>
                             <!-- /.table-responsive -->
@@ -150,6 +149,7 @@
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
+        
         
         <div class="row">
                 <div class="col-lg-12">
@@ -163,9 +163,8 @@
 
                             <!--a class="btn btn-default btn-md btn-block"  href="../recommend_app/<?php echo $rec['application_id']?>"><b>Recommend</b></a-->
                                 <!--button type="submit" class="btn btn-default">Submit Button</button-->
-                                <input type="submit" class="btn btn-default"  formaction="../approve_app/<?php echo $rec['application_id']?>" value="<?php if($rec['status'] == 'Awaiting Recommendation') echo "Recommend and Approve";
-                            else echo "Approve"; ?>" />
-                                <input type="submit" class="btn btn-default"  formaction="../reject_apr_app/<?php echo $rec['application_id'] ?>" value="Reject" />
+                                <input type="submit" class="btn btn-default"  formaction="../recommend_app/<?php echo $rec['application_id']?>" value="Recommend"/>
+                                <input type="submit" class="btn btn-default"  formaction="../reject_app/<?php echo $rec['application_id']?>" value="Reject" />
                             </form>
                         </div>
                         
@@ -177,38 +176,12 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                         <!-- add this attribute to button to open in new tab target="_blank" -->
-                            <!--a class="btn btn-default btn-md btn-block"  href="../reject_app/"><b>Reject</b></a>
+                            <!--a class="btn btn-default btn-md btn-block"  href="../reject_app/<?php echo $rec['application_id'] ?>"><b>Reject</b></a>
                         </div>
                        
                 </div-->
                 <!-- /.col-lg-6 -->
             </div>
-        <!-- add this attribute to button to open in new tab target="_blank" -->
-        <!--div class="row">
-                <div class="col-lg-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <a class="btn btn-default btn-md btn-block"  href="../approve_app/<!?php echo $rec['application_id']?>"><b>
-                            <!?php if($rec['status'] == 'Awaiting Recommendation') echo "Recommend and Approve";
-                            else echo "Approve"; ?>
-                            
-                            </b></a>
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-               
-                <div class="col-lg-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                        
-                            <a class="btn btn-default btn-md btn-block"  href="../reject_apr_app/<!?php echo $rec['application_id'] ?>"><b>Reject</b></a>
-                        </div>
-                       
-                </div>
-                
-            </div-->
             <!-- /.row -->
                 </div>
                 </div>
@@ -233,21 +206,21 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../../vendor/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+    <script src="../../vendor/metisMenu/metisMenu.min.js"></script>
 
     <!-- DataTables JavaScript -->
-    <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
-    <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+    <script src="../../vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="../../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+    <script src="../../vendor/datatables-responsive/dataTables.responsive.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
+    <script src="../../dist/js/sb-admin-2.js"></script>
 
     <!-- Page-Level Demo Scripts - Tables - Use for reference -->
     <script>
