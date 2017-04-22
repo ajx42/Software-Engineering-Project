9cadb8865844c9734cf94c5765896dbac4409b3d
Aditya Jain <adityajain.aj10@gmail.com>
2017-04-23 01:41:58 +0530

    Update joining_report.php

--- sep/src/public/joining_report.php
+++ sep/src/public/joining_report.php
@@ -33,16 +33,16 @@ session_start();
     <title>Submit Joining Report</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="../../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../../dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -56,7 +56,7 @@ session_start();
 <body>
 
     <div id="wrapper">
-        <?php include 'bars.php' ?>
+        <?php include 'bars_internal.php' ?>
         <div id="page-wrapper">
 
             <div class="row">
@@ -75,12 +75,15 @@ session_start();
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-lg-6">
-                                    <form role="form" action="./submit_join" method="post">
+                                    <form role="form" action="../submit_join" method="post">
                                         <div class="form-group">
                                             <label>Name of Applicant</label>
                                             <input class="form-control" name="name"  value = "<?php echo $rec['name']?>" disabled>
                                         </div>
-
+                                        <div class="form-group">
+                                            <label>For Application ID</label>
+                                            <input class="form-control" name="for_app_id" required value="<?php echo $rec['application_id']?>"readonly>
+                                        </div>
 
                                         <div class="form-group">
                                             <label>Designation</label>
@@ -101,16 +104,7 @@ session_start();
                                                 <option>Other</option>
                                             </select>
                                         </div>
-                                        <div class="form-group">
-                                            <label>Nature of Leave</label>
-                                            <select id="type_of_leave" class="form-control" name="nature" onClick="disabling();"   required>
-                                                <option>CL</option>
-                                                <option>HPL</option>
-                                                <option>EL</option>
-                                                <option>Vacation</option>
-                                                <option>Other</option>
-                                            </select>
-                                        </div>
+                                        
                                         
                                         <!--div class="form-group">
                                             <label>Recommending Authority</label>
@@ -146,6 +140,11 @@ session_start();
                                                 <option>Afternoon</option>
                                                 </select>
                                             </div>
+                                            <div class="form-group">
+                                            <label>Nature of Leave</label>
+                                            <input class="form-control" name="nature"  value ="<?php echo $rec['nature']?>" required readonly>
+                                             
+                                        </div>
                                             
                                        <button type="submit" class="btn btn-default">Submit Button</button>
                                         <button type="reset" class="btn btn-default">Reset Button</button>
@@ -170,16 +169,16 @@ session_start();
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../../vendor/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+    <script src="../../vendor/metisMenu/metisMenu.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
+    <script src="../../dist/js/sb-admin-2.js"></script>
 
 </body>
 
