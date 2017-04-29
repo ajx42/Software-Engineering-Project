93cb7bae5ec61c442c586e0d4e74fa9b00adf7e9
Aditya Jain <adityajain.aj10@gmail.com>
2017-04-30 02:10:40 +0530

    final update

--- sep/src/public/add_new_account.php
+++ sep/src/public/add_new_account.php
@@ -1,138 +1,195 @@
- <?php
-    session_start();
-    if(!isset($_SESSION['username'])) http_get("./");
-    else $myname = $_SESSION['myname'];
+<?php
+session_start();
 ?>
 
-<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="./admin.php">Hi! <?php echo $myname; ?></a>
-            </div>
-            <!-- /.navbar-header -->
-            <!-- template hai sab -->
-            <ul class="nav navbar-top-links navbar-right">
-                
-                
-                <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="./admin"><i class="fa fa-user fa-fw"></i> Site News</a>
-                        </li>
-                        <!--li><a href="./settings"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li-->
-                        <li class="divider"></li>
-                        <li><a href="./"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-            <!-- default template khatam -->
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
-                         <li>
-                            <a href="./admin"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="./add_news"><i class="fa fa-dashboard fa-fw"></i> Add Site News</a>
-                        </li>
-                        <li>
-                            <a href="./view_all_apps"><i class="fa fa-edit fa-fw"></i> All Applications</a>
-                        </li>
-                        <li>
-                            <a href="./view_all_users"><i class="fa fa-edit fa-fw"></i> All Users</a>
-                        </li>
-                        <li>
-                          <a href="./settings"><i class="fa fa-wrench fa-fw"></i> Account Settings</a>
-                        </li>
-                        <li>
-                          <a href="./add_new_account_holder"><i class="fa fa-wrench fa-fw"></i> Create New Account</a>
-                        </li>
-                        <!--li>
-                            <a href="./balance"><i class="fa fa-dashboard fa-fw"></i> Leave Balance</a>
-                        </li>
-                         <li>
-                            <a href="./my_leaves"><i class="fa fa-dashboard fa-fw"></i> Leave History</a>
-                        </li>
-                        <li>
-                            <a href="./settings"><i class="fa fa-wrench fa-fw"></i> Account Settings</a>
-                        </li-->
+<head>
 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-                        <!--li>
-                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i>Charts<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="flot.html">Flot Charts</a>
-                                </li>
-                                <li>
-                                    <a href="morris.html">Morris.js Charts</a>
-                                </li>
-                            </ul>
-                            
-                        </li-->
-                        
-                        
-                        
-                        
-                       
-                        <!--li>
-                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Third Level <span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                    </ul>
-                                   
-                                </li>
-                            </ul>
-                           
-                        </li-->
-                        
-                    </ul>
+    <title>ADD new acount</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+        <?php include 'bars2.php' ?>
+        <div id="page-wrapper">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header"> Add New Account</h1>
                 </div>
-                <!-- /.sidebar-collapse -->
+                <!-- /.col-lg-12 -->
             </div>
-            <!-- /.navbar-static-side -->
-        </nav>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                           Enter details of new account holder
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <form role="form" action="./submit_add_new_member" method="post">
+                                        <div class="form-group">
+                                            <label>Email-id / Username</label>
+                                            <input class="form-control" name="username" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Name</label>
+                                            <input class="form-control" name="name" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Type of Account</label>
+                                            
+                                            <select class="form-control" id="presuf3" name="type" required>
+                                                <option>General User</option>
+                                                <option>Approving Authority</option>
+                                                <option>Recommending Authority</option>
+                                                <option>Administrator</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Department</label>
+                                            
+                                            <select class="form-control" id="presuf3" name="department" >
+                                                <?php
+                                                    while($depr = mysqli_fetch_assoc($dep)){
+                                                ?>
+                                                <option>
+                                                <?php echo $depr['department'];?>
+                                                </option>
+                                                <?php
+                                                    } 
+                                                ?>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Approving Authority</label>
+                                            
+                                        	<select class="form-control" id="presuf3" name="approving" >
+                                                <?php
+                                                    while($aprr = mysqli_fetch_assoc($apr)){
+                                                ?>
+                                                <option>
+                                                <?php echo $aprr['name']?>
+                                                </option>
+                                                <?php
+                                                    } 
+                                                ?>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Preliminary CL</label>
+                                            <input class="form-control" name="cl" required>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <label>Preliminary HPL</label>
+                                            <input class="form-control" name="hpl" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Preliminary Vacation</label>
+                                            <input class="form-control" name="vacation" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Preliminary EL</label>
+                                            <input class="form-control" name="el" required>
+                                        </div>
+                                        <!--div class="form-group">
+                                            <label>Recommending Authority</label>
+                                            <select class="form-control" name="recommending_auth" required>
+
+                                                <option>cse150001001@iiti.ac.in</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </select>
+                                        </div-->
+                                      <input type="submit" class="btn btn-lg btn-success btn-block">  
+                                        
+                                   </form>     
+                                    
+                                </div>
+                                <!-- /.col-lg-6 (nested) -->
+                                <!--div class="col-lg-6">
+                                            <div class="form-group">
+                                            <label>Period of Leave</label>
+                                            <br><label>From: </label>
+                                            <input type="date" id="some" class="form-control" name="period_from" required>
+                                            <label>To: </label>
+                                            <input type="date" class="form-control" name="period_to" required>
+                                        </div>
+                                            
+                                            <div class="form-group">
+                                                <label>Report to duty with effect from: </label>
+                                                <select class="form-control" name="recommending_auth" required>
+
+                                                <option>Forenoon</option>
+                                                <option>Afternoon</option>
+                                                </select>
+                                            </div>
+                                            
+                                       <button type="submit" class="btn btn-default">Submit Button</button>
+                                        <button type="reset" class="btn btn-default">Reset Button</button>
+                                    </form>
+                                    
+                                </div-->
+                                <!-- /.col-lg-6 (nested) -->
+                            </div>
+                            <!-- /.row (nested) -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
